4f8e84be30fdd7293e41fa4f06cfa5144de3de61
Patrick Dullano <PWDullano@gmail.com>
2016-03-16 22:13:08 -0600

    viewing all boards updates in real time when  you create a new one

--- public/javascripts/controllers/main_controller.js
+++ public/javascripts/controllers/main_controller.js
@@ -10,8 +10,11 @@ app.controller("MainController", ['$scope', 'BoardsService', function ($scope, B
   $scope.board = {};
   $scope.addBoard = function () {
     var newBoard = $scope.board;
-    console.log(newBoard);
     BoardsService.addBoard(newBoard).then(function (stuff) {
+      BoardsService.all().then(function (boards) {
+        $scope.boards = boards.data;
+      });
+      $scope.boards.push(stuff.title)
       console.log('we have received stuff ', stuff);
     });
   }
