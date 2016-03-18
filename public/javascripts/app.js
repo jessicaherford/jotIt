65f85f09698e74ee38f3d98f04960013c288f790
Patrick Dullano <PWDullano@gmail.com>
2016-03-18 08:16:42 -0600

    dont know what i did

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -14,7 +14,7 @@ app.config(function($routeProvider, $locationProvider) {
         templateUrl: '/partials/notes.html',
         controller: 'BoardShowController'
       })
-      .when('/boards/:id/chatroom', {
+      .when('/chatroom', {
         templateUrl: '/partials/chatroom.html',
         controller: 'ChatController'
       })
